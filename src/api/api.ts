b9692abecf51fe7f3289cfa7090b412242073a88
ren <drrr0502@gmail.com>
2022-07-12 17:04:52 +0900

    fetch or null 修正

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,9 @@
-
+const env = import.meta.env.MODE 
+type Status = {
+    status: number
+}
 export async function fetchJSON<T>(path: string, options: RequestInit): Promise<T> {
-    if (import.meta.env.MODE === "development") path = `http://hogehoge.com/${path}`
+    if (env === "development") path = `http://hogehoge.com/${path}`
     else path = `http://localhost:8080/${path}`
     const res = await fetch(path, options)
     
@@ -11,14 +14,17 @@ export async function fetchJSON<T>(path: string, options: RequestInit): Promise<
     path: string,
     options: RequestInit,
     allowedStatusCodes?: number[],
-  ): Promise<T | null> {
-    if (import.meta.env.MODE === "development") path = `http://hogehoge.com/${path}`
+  ): Promise<T | Status| null> {
+    if (env === "development") path = `http://hogehoge.com/${path}`
     else path = `http://localhost:8080/${path}`
     allowedStatusCodes = allowedStatusCodes || [404]
     const res = await fetch(path, options)
   
-    if (res.status === 404 || allowedStatusCodes.includes(res.status)) {
+    if (res.status === 404) {
       return null
+    }
+    if (allowedStatusCodes.includes(res.status)){
+        return {status:res.status} as Status
     }
   
     return (await res.json()) as T
